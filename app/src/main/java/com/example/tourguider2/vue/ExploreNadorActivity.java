f7a1ffeeca97f8083cef511ee145b34c7bdb77d7
ELRHAOUAT <elghaouat.ma@gmail.com>
2020-05-11 05:12:12 +0000

    correction BottomNavigationView

--- app/src/main/java/com/example/tourguider2/vue/ExploreNadorActivity.java
+++ app/src/main/java/com/example/tourguider2/vue/ExploreNadorActivity.java
@@ -1,10 +1,14 @@
 package com.example.tourguider2.vue;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.tourguider2.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ExploreNadorActivity extends AppCompatActivity {
 
@@ -12,5 +16,32 @@ public class ExploreNadorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore_nador);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+        bottomNavigationView.setItemIconTintList(null);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ExploreFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_explore:
+                            selectedFragment = new ExploreFragment();
+                            break;
+                        case R.id.nav_map:
+                            selectedFragment = new MapFragment();
+                            break;
+                        case R.id.nav_wish_list:
+                            selectedFragment = new WishListFragment();
+                            break;
+                        case R.id.nav_plans:
+                            selectedFragment = new PlansFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+            };
 }
